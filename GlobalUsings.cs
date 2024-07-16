04e11659e048e0ea20879037c602058bce9cca9f
sunamo.cz <radek.jancik@sunamo.cz>
2024-07-16 19:04:19 +0200

    Before create public all except of _sunamo

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -4,6 +4,4 @@ global using System.Text;
 global using System;
 global using SunamoCssGenerator.Data;
 global using SunamoCssGenerator.Enums;
-global using SunamoCssGenerator.Extensions;
-
 global using SunamoCssGenerator._sunamo;
