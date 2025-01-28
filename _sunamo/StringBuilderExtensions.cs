c2dfd7739e2c0ad5ccb7c33b7a1cd2e90fd2c904
sunamo.cz <radek.jancik@sunamo.cz>
2025-01-28 09:44:31 +0100

    Buildable, removed unused internal methods in _sunamo

--- _sunamo/StringBuilderExtensions.cs
+++ _sunamo/StringBuilderExtensions.cs
@@ -4,15 +4,6 @@ internal static class StringBuilderExtensions
 {
     #region For easy copy
 
-    internal static void TrimEnd(this StringBuilder sb)
-    {
-        var length = sb.Length;
-        for (var i = length - 1; i >= 0; i--)
-            if (char.IsWhiteSpace(sb[i]))
-                sb.Remove(i, 1);
-            else
-                break;
-    }
 
     #endregion
 }
